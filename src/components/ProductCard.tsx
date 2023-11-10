5647bdfa2a5d17cdb6c154cafa9897ab530fe542
mohamed-mahmoud24 <mmooddyy184@gmail.com>
2023-11-10 23:17:23 +0200

    Enhance product card style

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -11,33 +11,40 @@ const ProductCard = ({ product }: IProps) => {
     const { title, category, colors, description, imageURL, price } = product;
     return (
         // <div className=" border border-white  text-white text-center rounded-md text-sm">
-        <div className="border rounded-md p-2 flex flex-col mb-4 max-w-sm md:max-w-lg mx-auto md:mx-0">
+        <div className="max-w-sm md:max-w-lg mx-auto md:mx-0 border rounded-md p-2 flex flex-col space-y-3">
             <Image
                 imageURL={imageURL}
                 alt={category.name}
                 className="rounded-md h-52 w-full lg:object-cover"
             />
-            <h3>{title}</h3>
-            <p>{txtSlicer(description)}</p>
-            <div className="flex space-x-2 items-center my-5">
+            <h3 className="text-lg font-semibold">{title}</h3>
+            <p className="text-sm text-gray-500 break-words">
+                {txtSlicer(description)}
+            </p>
+            <div className="flex items-center flex-wrap space-x-1">
                 {colors.length
                     ? colors.map((color) => (
                           <span
                               style={{
                                   background: color,
                               }}
-                              className="w-5 h-5 rounded-full cursor-pointer"
+                              className="block w-5 h-5 rounded-full cursor-pointer mb-1"
                           ></span>
                       ))
                     : "Not available colors!"}
             </div>
             <div className="flex items-center justify-between">
-                <span>${price}</span>
-                <Image
-                    className="w-10 h-10 rounded-full object-cover"
-                    imageURL={category.imageURL}
-                    alt={category.name}
-                />
+                <span className="text-lg text-indigo-600 font-semibold">
+                    ${price}
+                </span>
+                <div className="flex space-x-1 items-center">
+                    <span>{category.name}</span>
+                    <Image
+                        className="w-10 h-10 rounded-full object-cover"
+                        imageURL={category.imageURL}
+                        alt={category.name}
+                    />
+                </div>
             </div>
             <div className="flex items-center justify-between space-x-2 mt-5">
                 <Button className="bg-indigo-700 " width="w-full">
