b5e60f70966e1b2b16a16f20acf5f3e2637aafce
mohamed-mahmoud24 <mmooddyy184@gmail.com>
2023-11-11 01:18:57 +0200

    custom modal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,57 @@
+import { useState } from "react";
 import ProductCard from "./components/ProductCard";
+import Modal from "./components/UI/Modal";
 import { productList } from "./data";
+import Button from "./components/UI/Button";
+
 const App = () => {
+    /* STATE */
+    const [isOpen, setIsOpen] = useState(false);
+
+    /* HANDLER */
+    function closeModal() {
+        setIsOpen(false);
+    }
+
+    function openModal() {
+        setIsOpen(true);
+    }
     const renderProductList = productList.map((product) => (
         <ProductCard product={product} key={product.id} />
     ));
     return (
         <main className="container ">
+            <Button
+                className="bg-indigo-700 hover:bg-indigo-800 "
+                width="w-full"
+                onClick={() => openModal()}
+            >
+                ADD
+            </Button>
             <div className="grid gap-2 md:gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3  xl:grid-cols-4 m-5 p-2 rounded-md">
                 {renderProductList}
             </div>
+            <Modal
+                title="ADD A NEW PRODUCT"
+                closeModal={closeModal}
+                isOpen={isOpen}
+            >
+                <div className="flex items-center space-x-3">
+                    <Button
+                        className="bg-indigo-700 hover:bg-indigo-900"
+                        width="w-full"
+                    >
+                        SUBMIT
+                    </Button>
+                    <Button
+                        className="bg-gray-400 hover:bg-gray-500"
+                        width="w-full"
+                        onClick={() => closeModal()}
+                    >
+                        CANCEL
+                    </Button>
+                </div>
+            </Modal>
         </main>
     );
 };
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -28,6 +28,7 @@ const ProductCard = ({ product }: IProps) => {
                               style={{
                                   background: color,
                               }}
+                              key={color}
                               className="block w-5 h-5 rounded-full cursor-pointer mb-1"
                           ></span>
                       ))
@@ -47,10 +48,13 @@ const ProductCard = ({ product }: IProps) => {
                 </div>
             </div>
             <div className="flex items-center justify-between space-x-2 mt-5">
-                <Button className="bg-indigo-700 " width="w-full">
+                <Button
+                    className="bg-indigo-700 hover:bg-indigo-900"
+                    width="w-full"
+                >
                     EDIT
                 </Button>
-                <Button className="bg-red-700 " width="w-full">
+                <Button className="bg-red-700 hover:bg-red-900" width="w-full">
                     DELETE
                 </Button>
             </div>
