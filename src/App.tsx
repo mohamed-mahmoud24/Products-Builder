83ce47821839e84574b4dff393573aef265800d7
mohamed-mahmoud24 <mmooddyy184@gmail.com>
2023-11-13 01:39:45 +0200

    product state

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,55 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import ProductCard from "./components/ProductCard";
 import Modal from "./components/UI/Modal";
 import { formInputsList, productList } from "./data";
 import Button from "./components/UI/Button";
 import Input from "./components/UI/Input";
+import { IProduct } from "./interfaces";
 
 const App = () => {
     /* STATE */
+    const [product, setProduct] = useState<IProduct>({
+        title: "",
+        description: "",
+        imageURL: "",
+        price: "",
+        colors: [],
+        category: {
+            name: "",
+            imageURL: "",
+        },
+    });
     const [isOpen, setIsOpen] = useState(false);
 
     /* HANDLER */
-    function closeModal() {
-        setIsOpen(false);
-    }
-
-    function openModal() {
-        setIsOpen(true);
-    }
+    const closeModal = () => setIsOpen(false);
+    const openModal = () => setIsOpen(true);
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        const { value, name } = e.target;
+        setProduct({ ...product, [name]: value });
+    };
 
     /* RENDER */
     const renderProductList = productList.map((product) => (
         <ProductCard product={product} key={product.id} />
     ));
     const renderFormInputList = formInputsList.map((input) => (
-        <div className="flex flex-col">
+        <div key={input.id} className="flex flex-col">
             <label
                 className="mb-1 text-sm font-medium text-gray-700"
                 htmlFor={input.id}
             >
                 {input.label}
             </label>
-            <Input type={input.type} name={input.name} id={input.id} />
+            {/* LINE BELOW IN WRONG NOW FIX IT! */}
+            {/* <Input type="text" id={input.id} name={input.name} value={product[]} onChange={onChangeHandler} /> */}
+            <Input
+                type="text"
+                id={input.id}
+                name={input.name}
+                value={""}
+                onChange={onChangeHandler}
+            />
         </div>
     ));
 
